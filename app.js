3b13bf15e6dabfe9307bed727217688a2dd1efd5
Ashok Kumar <agandamaneni@opteamix.com>
2017-07-24 18:05:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const calc = require('./calc');
+
+calc.add(1, 2);
+calc.sub(4,6);
---
+++ calc.js
@@ -0,0 +1,12 @@
+function add(a, b){
+    console.log('Value of a + b = ', a + b);
+}
+
+function sub(a,  b){
+    console.log('Value of a - b = ', a - b);
+}
+
+module.exports = {
+    add : add,
+    sub : sub 
+}
